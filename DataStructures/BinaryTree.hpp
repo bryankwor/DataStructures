7499119f39ee008ee127f7d14fdd3c6aacc9dec6
bworrell <bryankwor@gmail.com>
2015-01-02 17:19:38 -0500

    Fixed templating errors.

--- DataStructures/BinaryTree.hpp
+++ DataStructures/BinaryTree.hpp
@@ -27,10 +27,10 @@ template <class T>
 class BST
 {
 private:
-	TreeNode* _root;
+	TreeNode<T>* _root;
 
 private:
-	TreeNode* getItemPositionInTree(const T &item, TreeNode* parent);
+	TreeNode<T>* getItemPositionInTree(const T &item, TreeNode<T>* parent);
 
 public:
 	// Constructors / Desctructors.
@@ -42,17 +42,17 @@ public:
 	void insertItem(const T &item);
 	void removeItem(const T &item);
 
-	void findMaxAtLevel(TreeNode *p, const int &level) const;
-	void preOrder(TreeNode *p) const;
-	void inOrder(TreeNode *p) const;
-	void postOrder(TreeNode *p) const;
+	void findMaxAtLevel(TreeNode<T> *p, const int &level) const;
+	void preOrder(TreeNode<T> *p) const;
+	void inOrder(TreeNode<T> *p) const;
+	void postOrder(TreeNode<T> *p) const;
 };
 
 template <class T>
-TreeNode* getItemPositionInTree(const T &item, TreeNode* parent)
+TreeNode<T>* BST<T>::getItemPositionInTree(const T &item, TreeNode<T>* parent)
 {
 	bool found = false;
-	TreeNode *current = root;
+	TreeNode<T> *current = root;
 
 	// Traverse tree to find item.
 	while (current)
@@ -84,11 +84,11 @@ TreeNode* getItemPositionInTree(const T &item, TreeNode* parent)
 }
 
 template <class T>
-bool contains(const T &item) const
+bool BST<T>::contains(const T &item) const
 {
 	// TODO: This can be optimized by having the insert function store the value of each new item added to the tree in a hash table.
 	// Then this function can just look up the value in the hash table in constant time, provided there are no collisions.
-	TreeNode *current = root;
+	TreeNode<T> *current = root;
 
 	// Traverse tree to find item.
 	while (current)
@@ -114,9 +114,9 @@ bool contains(const T &item) const
 }
 
 template <class T>
-void BST::insertItem(const T &item)
+void BST<T>::insertItem(const T &item)
 {
-	TreeNode* newNode = new TreeNode();
+	TreeNode<T>* newNode = new TreeNode();
 	newNode->setValue(item);
 
 	// Check if the tree is new.
@@ -127,8 +127,8 @@ void BST::insertItem(const T &item)
 	else
 	{
 		// Create a pointer to traverse the tree, starting at the root.
-		TreeNode *parent = nullptr;
-		TreeNode *current = root;
+		TreeNode<T> *parent = nullptr;
+		TreeNode<T> *current = root;
 
 		// Find correct position for new node; smaller elements go left, larger elements go right.
 		while (current)
@@ -157,7 +157,7 @@ void BST::insertItem(const T &item)
 }
 
 template <class T>
-void BST::removeItem(const T &item)
+void BST<T>::removeItem(const T &item)
 {
 	// Check if the tree is empty or doesn't contain the item.
 	if (root == nullptr || !contains(item))
@@ -167,8 +167,8 @@ void BST::removeItem(const T &item)
 	// Item exists. Get its position in the tree and handle 3 deletion cases.
 	else
 	{
-		TreeNode* parent = nullptr;
-		TreeNode* current = getItemPositionInTree(item, parent);
+		TreeNode<T>* parent = nullptr;
+		TreeNode<T>* current = getItemPositionInTree(item, parent);
 
 		// Case 1: The item is in a leaf node.
 		if (current->getLeftChild() == nullptr && current->getRightChild() == nullptr)
@@ -227,7 +227,7 @@ void BST::removeItem(const T &item)
 		// Case 3: The item is in a node with 2 children.
 		else
 		{
-			TreeNode *check;
+			TreeNode<T> *check;
 			check = current->getRightChild();
 
 			// Does right child have any children?
@@ -243,8 +243,8 @@ void BST::removeItem(const T &item)
 				// If the right child has a left child, move all the way down left to locate smallest item
 				if ((current->getRightChild())->getLeftChild() != nullptr)
 				{
-					TreeNode *leftCurrent;
-					TreeNode *leftCurrentPtr;
+					TreeNode<T> *leftCurrent;
+					TreeNode<T> *leftCurrentPtr;
 					leftCurrentPtr = current->getRightChild();
 					leftCurrent = (current->getRightChild())->getLeftChild();
 
@@ -260,7 +260,7 @@ void BST::removeItem(const T &item)
 
 				else
 				{
-					TreeNode *temp;
+					TreeNode<T> *temp;
 					temp = current->getRightChild();
 					current->setValue(temp->getValue());
 					current->setRightChild(temp->getRightChild());
@@ -272,7 +272,7 @@ void BST::removeItem(const T &item)
 }
 
 template <class T>
-void BST::findMaxAtLevel(TreeNode *p, const int &level) const
+void BST<T>::findMaxAtLevel(TreeNode<T> *p, const int &level) const
 {
 	// TODO: Fix base case.
 	if (level == 0)
@@ -303,7 +303,7 @@ void BST::findMaxAtLevel(TreeNode *p, const int &level) const
 
 // Root, Left, Right.
 template <class T>
-void BST::preOrder(TreeNode *p) const
+void BST<T>::preOrder(TreeNode<T> *p) const
 {
 	if (root != nullptr)
 	{
@@ -327,7 +327,7 @@ void BST::preOrder(TreeNode *p) const
 
 // Left, Root, Right
 template <class T>
-void BST::inOrder(TreeNode *p) const
+void BST<T>::inOrder(TreeNode<T> *p) const
 {
 	if (p != nullptr)
 	{
@@ -351,7 +351,7 @@ void BST::inOrder(TreeNode *p) const
 
 // Left, Right, Root
 template <class T>
-void BST::postOrder(TreeNode *p) const
+void BST<T>::postOrder(TreeNode<T> *p) const
 {
 	if (p != nullptr)
 	{
